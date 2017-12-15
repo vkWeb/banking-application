cfb68ea80ac537eef1938d4c1218e872afb85020
csyash <34562259+csyash@users.noreply.github.com>
2017-12-15 21:07:18 +0530

    ADDED  5-FAQs

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,4 +1,64 @@
-void main()
-{
-cout<<"Hello World!"<<endl;
-}
+#include<iostream.h>
+#include<conio.h>
+  void main()
+  {
+	  cout << " \t  WELCOME TO OUR BANK MANAGMENT SYSTEM      " ; getch();
+		  cout<<endl;
+		 cout<<"         "; for(int i=0;i<=37;i++)
+		  cout<<"_";
+	  cout<<"\n\n\n \t   HERE ARE THE FAQS "; getch();
+	  cout<<endl;
+		 cout<<"         "; for( i=0;i<=20;i++)
+		  cout<<"_";
+
+
+
+
+	  cout<<"\n\n\n1). What should I do in case I am unable to access my Company's Current Account?\n";
+	  cout<<"Ans   You are unable to access your company’s Current Account, it may be because     you have entered an incorrect Customer Identification Number (Cust ID) to       log into your NetBanking Account."
+<<"While opening Accounts of Companies,           Customer IDs are created in the name of the Company as well as each of the      Authorized Signatory(ies). The Authorised Signatory(ies) is distinct and         has to   be used to access NetBanking for the Company’s Account.";
+			  cout<<endl;
+		 for( i=0;i<=78;i++)
+		 { cout<<"_";}
+
+	  cout<<". \n\n2). What transactions are included as a part of the free inter-city clearing ?\n\n";
+
+	  cout
+					 <<"Ans   The following transactions comprise part of the free Inter-city clearing        Limit:\n"
+					  <<"     *Cheques deposited into your account in other cities "
+					  <<"\n     *Inter-city payable-at-par cheque issuances. ";
+
+					  cout<<endl;
+		 for( i=0;i<=78;i++)
+		 { cout<<"_";}
+
+		cout<<"\n\n3). Must I open Premium Current account in each location where I have my company     office?\n\n";
+		cout<<"Ans  No. As a Premium Current account holder you have the advantage of a single      account, multi-location banking facility.";
+
+		  cout<<endl;
+		  for(i=0;i<=79;i++)
+
+
+		  cout<<"_";
+
+		  cout<<"\n\n\n3). How do I know my beneficiary's details for a cheque issued in my name          (Kisan Club Savings account)?\n";
+		  cout<<"Ans  To know your beneficiary’s details for a cheque issued by you / issuer name     for a cheque credited in your account, please contact any of our bank           branches.";
+
+									  cout<<endl;
+		 for( i=0;i<=78;i++)
+		 { cout<<"_";}
+
+		 cout<<"\n\n\n4).  Can a salary account have a joint applicant?\n";
+		 cout<<"Ans  Yes, parents, spouse, child or sibling can be a joint applicant to an           account. The joint applicant will need to submit a valid photo ID and           address proof.";
+
+										  cout<<endl;
+		 for( i=0;i<=78;i++)
+		 { cout<<"_";}
+
+		 cout<<"\n\n\n5). How does Debit card work? What should I know?\n";
+		 cout<<"Ans  When you want to make a purchase, just present your card to the store           merchant. They will swipe the card through an electronic reader, and enter      the  amount to be paid. You will need to sign a transaction slip, and you       should be given a copy. After this, your HDFC Bank account is debited           automatically.";
+
+
+
+							getch();
+							 }
