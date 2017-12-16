2dbee955e3091e5564022ef1c9e17a01c29ae625
Vivek Agrawal <vivekmittalagrawal@gmail.com>
2017-12-16 15:40:22 +0530

    updated main screen

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@ using namespace std;
 //Included files
 #include <iostream>
 #include <string>
+#include <cstdlib>
 
 //structures and objects
 struct profile_details
@@ -10,7 +11,7 @@ struct profile_details
     string username;
     string pass;
     string email;
-}profile;
+} profile;
 
 //function declarations
 void welcome_screen(profile_details profile);
@@ -19,8 +20,7 @@ void main_screen(profile_details profile);
 //start main()
 int main()
 {
-    cout<<"\t\t\t\t\t\t\t\t\t Welcome!\n\t\t\t\t\t\t\t\tBanking Humantics Version 1.0";
-    welcome_screen(profile);
+    //welcome_screen(profile);
     main_screen(profile);
     return 0;
 }
@@ -29,40 +29,51 @@ int main()
 void welcome_screen(profile_details profile)
 {
     int count = 0;
-    cout<<"\n------------";    
-    cout<<"\nInstructions:";
-    cout<<"\n------------";
-    cout<<"\n-> Here, you'll be setting up your profile as a banking officer.\n-> Your username can consist of alphanumeric characters and your Password must be atleast 6 characters long.\n-> Fields marked with '*' are necessary.";
-    cout<<"\n\n\n1. Enter Your Username*: ";cin>>profile.username;
-    cout<<"\n2. Enter Your Password*: ";
+    cout << "\t\t\t\t\t\t\t\t\t Welcome!\n\t\t\t\t\t\t\t\tBanking Humantics Version 1.0";
+    cout << "\n------------";
+    cout << "\nInstructions:";
+    cout << "\n------------";
+    cout << "\n-> Here, you'll be setting up your profile as a banking officer.\n-> Your username can consist of alphanumeric characters and your Password must be atleast 6 characters long.\n-> Fields marked with '*' are necessary.";
+    cout << "\n\n\n1. Enter Your Username*: ";
+    cin >> profile.username;
+    cout << "\n2. Enter Your Password*: ";
 
     do
     {
-        if(count > 0)
+        if (count > 0)
         {
-            cout<<":( Sorry! Your Password must be atleast six characters, please try again: ";
+            cout << ":( Sorry! Your Password must be atleast six characters, please try again: ";
         }
-        cin>>profile.pass;
+        cin >> profile.pass;
         count++;
-    }
-    while(profile.pass.length() < 6);
+    } while (profile.pass.length() < 6);
 
-    cout<<"\n3. Enter Your Email Address*: ";cin>>profile.email;
-    cout<<"\n\nVerifying details...Now, you're ready to go! Please, Press 'Enter' to continue.";
-    cin.ignore(); cin.ignore();
+    cout << "\n3. Enter Your Email Address*: ";
+    cin >> profile.email;
+    cout << "\n\nVerifying details...Now, you're ready to go! Please, Press 'Enter' to continue." << endl;
+    cin.ignore();
+    cin.ignore();
+    system("clear");
 }
 
-//main_screen function
+//main_screen function()
 void main_screen(profile_details profile)
 {
-    cout<<"\n------------";    
-    cout<<"\nInstructions:";
-    cout<<"\n------------";
-    cout<<"\n-> This is your Home Screen where you'll be carrying out all your work.\n-> .\n-> Fields marked with '*' are necessary.";
+    cout << "\t\t\t\t\t\t\t\t\t Home Screen\n\t\t\t\t\t\t\t\tBanking Humantics Version 1.0";
+    cout << "\n------------";
+    cout << "\nInstructions:";
+    cout << "\n------------";
+    cout << "\n-> This is your Home Screen where you'll be carrying out all your work.\n-> There are different sections of services you can work with.\n-> To select a particular service press the mentioned 'numeral' key and then to select an option press the mentioned 'alphabetical' key.\n-> Here's a quick example, to open a new account. You'll have to press '1' and then 'a'.";
+
+    cout << "\n\n\n1. Customer Centric Services: \t\t\t2. Transaction Services:";
+    cout << "\na) Open an bank account. \t\t\ta) Fund transfer to one of your own accounts.";
+    cout << "\nb) Close a bank account. \t\t\tb) Fund transfer to another account of same bank - Intra Banking Transfer.";
+    cout << "\nc) Get your Account statement. \t\t\tc) Fund transfer to another account of same bank - Intra Banking Transfer.";
+    cout << "\n\t\t\t\t\t\td) Fund transfer to another account of different bank - Inter Banking Transfer.";
+
+    cout << "\n\n3. Loan Services: \t\t\t\t4.Customer Care, Support and Helpdesk: ";
+    cout << "\na) Startup Buisness Loan.\t\t\ta)Account issues.";
+    cout << "\nb) Loan for girl education.\t\t\tb)Debit or Credit Card issues.";
+    cout << "\nc) Loan for farmers. \t\t\t\tc)Net banking issues.";
+    cout << "\nd) Indian citizen loan. \t\t\td)Other support.";
 }
-
-
-
-
-
-
